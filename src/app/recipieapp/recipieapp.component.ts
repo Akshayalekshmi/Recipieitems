297b901ad9e914afc81f66ff0f1b476f5bf61f63
Akshayalekshmi <akshayamakairam@gmail.com>
2022-07-02 16:05:56 +0530

    recipieitems

---
+++ src/app/recipieapp/recipieapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipieapp',
+  templateUrl: './recipieapp.component.html',
+  styleUrls: ['./recipieapp.component.css']
+})
+export class RecipieappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
